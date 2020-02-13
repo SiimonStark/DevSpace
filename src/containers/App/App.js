2fe761af5ee4339c2a4bdee540c1c4b6026c7ad4
SiimonStark <justinpyktel@gmail.com>
2020-02-13 12:51:44 -0500

    Setup structure, install Router, node-sass

---
+++ src/containers/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import loadable from '@loadable/component';
+
+// ?Placeholder? // const <Component> = loadable(() => import('./<Component>));
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
